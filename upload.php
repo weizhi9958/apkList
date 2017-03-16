1f91f4455af9728a11166c30347c6238160a89ca
1500380a <wzzeng@softmobile.com.tw>
2017-03-16 14:05:49 +0800

    update

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?
 if( isset($_FILES['files']) && $_FILES['files']['error']==0 ){
-	$upload_folder = dirname(dirname(__FILE__))."/yuanta";
+	$upload_folder = dirname(dirname(__FILE__))."/apkList";
 	$upload_path = $upload_folder."/apk/".$_FILES['files']['name'];
 	move_uploaded_file($_FILES['files']['tmp_name'], $upload_path);
 	echo '上傳成功！';
